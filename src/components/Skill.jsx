7ef794f98d2a21fc02e672d67d87f7038942935d
PatiphanAK <pakkahadsri@gmail.com>
2025-06-11 23:03:38 +0700

    remove

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -5,7 +5,6 @@ export default function Skill() {
     return (
       <>
       <div className="text-center">
-        <h2 className="text-4xl font-bold">Development Environment & Tooling</h2>
         <LogoBlock />
         <CoreTool />
       </div>
